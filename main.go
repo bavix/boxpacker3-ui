7fbf0d78ea99445ea1648b3a498bf054e531ef76
Maxim Babichev <maksim.babichev95@gmail.com>
2023-12-16 00:10:17 +0300

    refactoring

--- main.go
+++ main.go
@@ -2,35 +2,41 @@ package main
 
 import (
 	"embed"
-	"github.com/bavix/boxpacker3-ui/internal/app"
+	"errors"
 	"io/fs"
 	"log"
+	"net"
 	"net/http"
 	"os"
+
+	"github.com/bavix/boxpacker3-ui/internal/app"
 )
 
 //go:embed public
 var publicFiles embed.FS
 
 func main() {
-	publicFS := fs.FS(publicFiles)
-	publicContent, err := fs.Sub(publicFS, "public")
+	htmlContent, err := fs.Sub(publicFiles, "public")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	http.Handle("/", http.FileServer(http.FS(publicContent)))
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
+	http.Handle("/", http.FileServer(http.FS(htmlContent)))
 
 	http.HandleFunc("/bp3", app.Bp3Handle)
 	http.HandleFunc("/bp3boxes", app.Bp3DefaultBoxesHandle)
 
-	log.Printf("Listening on :%s...", port)
-	err = http.ListenAndServe(":"+port, nil)
-	if err != nil {
+	host, _ := os.LookupEnv("HOST")
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "8080"
+	}
+
+	addr := net.JoinHostPort(host, port)
+	log.Printf("Listening on %s...\n", addr)
+
+	err = http.ListenAndServe(addr, nil)
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
 		log.Fatal(err)
 	}
 }
